d2c7aa846559d281924adcaf0d6512ff479cf02a
susie-y <yangsuijia@gmail.com>
2022-12-29 14:09:52 +0800

    day1 comment

--- gee-web/day1-http-base/base3/main.go
+++ gee-web/day1-http-base/base3/main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"gee"
+	"net/http"
+)
+
 // $ curl http://localhost:9999/
 // URL.Path = "/"
 // $ curl http://localhost:9999/hello
@@ -7,13 +13,8 @@ package main
 // Header["User-Agent"] = ["curl/7.54.0"]
 // curl http://localhost:9999/world
 // 404 NOT FOUND: /world
-
-import (
-	"fmt"
-	"net/http"
-
-	"gee"
-)
+// 暂未提供超过标准库的功能
+// 参考gin的api设计，实现 静态路由配置。通过 new 一个 engine，把标准库的路由配置由GET/POST等方法代理， ListenAndServe封装成 engine.Run
 
 func main() {
 	r := gee.New()
